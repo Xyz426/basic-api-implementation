febe2af59398e01cfff25e5bd755b536abd15ef3
xyz <xyzzhybz@163.com>
2020-08-04 23:52:00 +0800

    finished addOneRsEvent

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,24 +1,41 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-    private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+    private List<RsEvent> rsList = init();
+
+    private List<RsEvent> init() {
+        rsList = new ArrayList<>();
+        rsList.add(new RsEvent("第一件事件","篮球"));
+        rsList.add(new RsEvent("第二件事件","游泳"));
+        rsList.add(new RsEvent("第三件事件","足球"));
+
+        return rsList;
+    }
+
 
     @GetMapping("rs/{index}")
-    public String shouldGetOneRsEvent(@PathVariable int index) {
+    public RsEvent shouldGetOneRsEvent(@PathVariable int index) {
         return rsList.get(index - 1);
     }
 
     @GetMapping("rs/list")
-    public String shouldGetRsEventBetween(@RequestParam int start, @RequestParam int end) {
-        return rsList.subList(start - 1, end).toString();
+    public List<RsEvent> shouldGetRsEventBetween(@RequestParam int start, @RequestParam int end) {
+        return rsList.subList(start - 1, end);
+    }
+
+    @PostMapping("rs/event")
+    public void shouldAddOneRsEvent(@RequestBody String rsEventString) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        RsEvent rsEvent = objectMapper.readValue(rsEventString, RsEvent.class);
+        rsList.add(rsEvent);
     }
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,18 @@
 package com.thoughtworks.rslist;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.api.RsEvent;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,15 +27,38 @@ class RsListApplicationTests {
 
     @Test
     void shouldGetOneRsEvent() throws Exception {
+
         mockMvc.perform(get("/rs/1"))
-                .andExpect(content().string("第一条事件"))
+                .andExpect(jsonPath("$.eventName",is("第一件事件")))
+                .andExpect(jsonPath("$.keyWord",is("篮球")))
                 .andExpect(status().isOk());
     }
 
     @Test
     void shouldGetRsEventBetween() throws Exception {
         mockMvc.perform(get("/rs/list?start=1&end=3"))
-                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
+                .andExpect(jsonPath("$[0].eventName",is("第一件事件")))
+                .andExpect(jsonPath("$[0].keyWord",is("篮球")))
+                .andExpect(jsonPath("$[1].eventName",is("第二件事件")))
+                .andExpect(jsonPath("$[1].keyWord",is("游泳")))
+                .andExpect(jsonPath("$[2].eventName",is("第三件事件")))
+                .andExpect(jsonPath("$[2].keyWord",is("足球")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldAddOneRsEvent() throws Exception {
+        RsEvent rsEvent = new RsEvent("第四件事件", "火山爆发");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestJson = objectMapper.writeValueAsString(rsEvent);
+
+        mockMvc.perform(post("/rs/event").content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/rs/4"))
+                .andExpect(jsonPath("$.eventName",is("第四件事件")))
+                .andExpect(jsonPath("$.keyWord",is("火山爆发")))
                 .andExpect(status().isOk());
     }
 }
