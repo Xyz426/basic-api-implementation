2dcb54f18dcef07483c2489d7f92280309d8ab41
xyz <xyzzhybz@163.com>
2020-08-05 14:22:58 +0800

    finished validation

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,35 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+    private List<RsEvent> rsList = new ArrayList<>();
+    private Map<String,User> userMap = new HashMap<>();
+
+
+    @GetMapping("rs/list")
+    public List<RsEvent> shouldGetRsEvent(){
+        return rsList;
+    }
+
+    @PostMapping("/rs/event")
+    public void shouldAddOneRsEvent(@RequestBody @Valid RsEvent rsEvent){
+        @Valid
+        User user = rsEvent.getUser();
+        if(!userMap.containsKey(user.getUserName())){
+            userMap.put(user.getUserName(),user);
+        }
+
+        rsList.add(rsEvent);
+    }
+
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,67 @@
+
 package com.thoughtworks.rslist;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.api.RsEvent;
+import com.thoughtworks.rslist.api.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RsListApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void shouldAddOneRsEvent() throws Exception {
+        User user = new User("xiaolizi", 21, "male", "xyz@123.com", "11234567890");
+
+        RsEvent rsEvent = new RsEvent("第一件热搜", "hhhh成功", user);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestJson = objectMapper.writeValueAsString(rsEvent);
+
+        mockMvc.perform(post("/rs/event").content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$[0].eventName", is("第一件热搜")))
+                .andExpect(jsonPath("$[0].keyWord", is("hhhh成功")))
+                .andExpect(jsonPath("$[0].user.userName", is("xiaolizi")))
+                .andExpect(status().isOk());
     }
 
+    @Test
+    void nameShouldNotLonger8() throws Exception {
+        User user = new User("xiaolizixxxx", 21, "male", "xyz@123.com", "11234567890");
+
+        RsEvent rsEvent = new RsEvent("第二件热搜", "hhhh失败", user);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestJson = objectMapper.writeValueAsString(rsEvent);
+
+        mockMvc.perform(post("/rs/event").content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void ageShouldBetween18and100() throws Exception {
+        User user = new User("xiaolizixxxx", 212, "male", "xyz@123.com", "11234567890");
+
+        RsEvent rsEvent = new RsEvent("第二件热搜", "hhhh失败", user);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestJson = objectMapper.writeValueAsString(rsEvent);
+
+        mockMvc.perform(post("/rs/event").content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
 }
