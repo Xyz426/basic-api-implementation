21607b71069b9f5c53f26e22ddecc7b278ad7bda
xyz <xyzzhybz@163.com>
2020-08-09 21:51:17 +0800

    finished 5

--- src/main/java/com/thoughtworks/rslist/repository/RsEventRepository.java
+++ src/main/java/com/thoughtworks/rslist/repository/RsEventRepository.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.rslist.repository;
 
 import com.thoughtworks.rslist.entity.RsEventEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
@@ -10,6 +11,8 @@ public interface RsEventRepository extends CrudRepository<RsEventEntity,Integer>
     @Override
     List<RsEventEntity> findAll();
 
+    @Query(value = "select * from rs_event where id between ?1 and ?2",nativeQuery = true)
+    List<RsEventEntity> findByRoundId(int start,int end);
 //    @Transactional
 //    void deleteAllByUserId(int userId);
 }
